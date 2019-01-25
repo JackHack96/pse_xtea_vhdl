ea0adf2d726594775b9905121158c0a6ec901b87
Matteo <matteoiervasi@gmail.com>
2019-01-25 12:21:46 +0100

    Updated design

--- XTEA.vhd
+++ XTEA.vhd
@@ -3,19 +3,20 @@ use IEEE.std_logic_1164.all;
 use IEEE.NUMERIC_BIT.all;
 
 package XTEA_PACK is
-	constant SIZE : integer := 32;
 	-- States definition
-	constant IDLE : integer := 0;
-	constant BUSY_KEY1 : integer := 1;
-	constant BUSY_KEY2 : integer := 2;
-	constant BUSY_ENC_INPUT : integer := 3;
-	constant BUSY_ENC1 : integer := 4;
-	constant BUSY_ENC2 : integer := 5;
-	constant BUSY_ENC_OUTPUT : integer := 6;
-	constant BUSY_DEC_INPUT : integer := 7;
-	constant BUSY_DEC1 : integer := 8;
-	constant BUSY_DEC2 : integer := 9;
-	constant BUSY_DEC_OUTPUT : integer := 10;
+	constant IDLE 				: UNSIGNED(3 downto 0) := "0000";
+	constant BUSY_KEY1 			: UNSIGNED(3 downto 0) := "0001";
+	constant BUSY_KEY2 			: UNSIGNED(3 downto 0) := "0010";
+	constant BUSY_ENC_INPUT 	: UNSIGNED(3 downto 0) := "0011";
+	constant BUSY_ENC1 			: UNSIGNED(3 downto 0) := "0100";
+	constant BUSY_ENC2 			: UNSIGNED(3 downto 0) := "0101";
+	constant BUSY_ENC_OUTPUT1	: UNSIGNED(3 downto 0) := "0110";
+	constant BUSY_ENC_OUTPUT2	: UNSIGNED(3 downto 0) := "0111";
+	constant BUSY_DEC_INPUT 	: UNSIGNED(3 downto 0) := "1000";
+	constant BUSY_DEC1 			: UNSIGNED(3 downto 0) := "1001";
+	constant BUSY_DEC2 			: UNSIGNED(3 downto 0) := "1010";
+	constant BUSY_DEC_OUTPUT1 	: UNSIGNED(3 downto 0) := "1011";
+	constant BUSY_DEC_OUTPUT2 	: UNSIGNED(3 downto 0) := "1100";
 end XTEA_PACK;
 
 library IEEE;
@@ -25,49 +26,36 @@ use WORK.XTEA_PACK.all;
 
 entity XTEA is
 	port (
-		clk : in bit;
-		rst : in bit;
-
-		text_input0 : in UNSIGNED (SIZE - 1 downto 0);
-		text_input1 : in UNSIGNED (SIZE - 1 downto 0);
-
-		key_input0 : in UNSIGNED (SIZE - 1 downto 0);
-		key_input1 : in UNSIGNED (SIZE - 1 downto 0);
-
-		data_output0 : out UNSIGNED (SIZE - 1 downto 0);
-		data_output1 : out UNSIGNED (SIZE - 1 downto 0);
-
-		input_ready : in bit;
-		mode : in bit;
-		output_ready : out bit
+		clk 			: in 	BIT;
+		rst 			: in 	BIT;
+		data_input 		: in 	UNSIGNED (63 downto 0);
+		data_output 	: out 	UNSIGNED (31 downto 0);
+		input_ready 	: in 	BIT;
+		mode 			: in 	BIT;
+		output_ready 	: out 	BIT
 	);
 end XTEA;
 
 architecture BEHAVIORAL of XTEA is
-	subtype STATUS_T is integer range 0 to 10;
-	subtype INTERNAL32_T is UNSIGNED (SIZE - 1 downto 0);
-	subtype INTERNAL64_T is UNSIGNED (63 downto 0);
+	subtype STATUS_T 		is UNSIGNED (3 downto 0);
+	subtype INTERNAL32_T 	is UNSIGNED (31 downto 0);
 
-	signal STATUS : STATUS_T;
-	signal NEXT_STATUS : STATUS_T;
-	signal KEY0 : INTERNAL32_T;
-	signal KEY1 : INTERNAL32_T;
-	signal KEY2 : INTERNAL32_T;
-	signal KEY3 : INTERNAL32_T;
-	signal TEXT0 : INTERNAL32_T;
-	signal TEXT1 : INTERNAL32_T;
-	signal RESULT0 : INTERNAL32_T;
-	signal RESULT1 : INTERNAL32_T;
+	signal STATUS 		: STATUS_T;
+	signal NEXT_STATUS 	: STATUS_T;
+	signal KEY0 		: INTERNAL32_T;
+	signal KEY1 		: INTERNAL32_T;
+	signal KEY2 		: INTERNAL32_T;
+	signal KEY3 		: INTERNAL32_T;
+	signal TEXT0 		: INTERNAL32_T;
+	signal TEXT1 		: INTERNAL32_T;
+	signal COUNTER 		: UNSIGNED(5 downto 0);
+	signal SUM 			: INTERNAL32_T;
 
-	signal COUNTER : UNSIGNED(6 downto 0);
-	signal SUM : INTERNAL64_T;
-
-	constant DELTA : INTERNAL32_T := "10011110001101110111100110111001";
-	constant ZERO32 : INTERNAL32_T := "00000000000000000000000000000000";
-	constant ZERO64 : INTERNAL64_T := "0000000000000000000000000000000000000000000000000000000000000000";
-	constant ONE : INTERNAL64_T := "0000000000000000000000000000000000000000000000000000000000000001";
-	constant TWO : INTERNAL64_T := "0000000000000000000000000000000000000000000000000000000000000010";
-	constant THREE : INTERNAL64_T := "0000000000000000000000000000000000000000000000000000000000000011";
+	constant DELTA 		: INTERNAL32_T := "10011110001101110111100110111001";
+	constant ZERO 		: INTERNAL32_T := "00000000000000000000000000000000";
+	constant ONE 		: INTERNAL32_T := "00000000000000000000000000000001";
+	constant TWO 		: INTERNAL32_T := "00000000000000000000000000000010";
+	constant THREE 		: INTERNAL32_T := "00000000000000000000000000000011";
 begin
 	-- FSM
 	process (STATUS, input_ready)
@@ -101,12 +89,14 @@ begin
 				NEXT_STATUS <= BUSY_ENC2;
 			when BUSY_ENC2 => 
 				if COUNTER = "100000" then
-					NEXT_STATUS <= BUSY_ENC_OUTPUT;
+					NEXT_STATUS <= BUSY_ENC_OUTPUT1;
 				else
 					NEXT_STATUS <= BUSY_ENC1;
 				end if;
-			when BUSY_ENC_OUTPUT => 
-				NEXT_STATUS <= BUSY_ENC_OUTPUT;
+			when BUSY_ENC_OUTPUT1 => 
+				NEXT_STATUS <= BUSY_ENC_OUTPUT2;
+			when BUSY_ENC_OUTPUT2 => 
+				NEXT_STATUS <= BUSY_ENC_OUTPUT1;
 			when BUSY_DEC_INPUT => 
 				if input_ready = '1' then
 					NEXT_STATUS <= BUSY_DEC1;
@@ -117,12 +107,14 @@ begin
 				NEXT_STATUS <= BUSY_DEC2;
 			when BUSY_DEC2 => 
 				if COUNTER = "100000" then
-					NEXT_STATUS <= BUSY_DEC_OUTPUT;
+					NEXT_STATUS <= BUSY_DEC_OUTPUT1;
 				else
 					NEXT_STATUS <= BUSY_DEC1;
 				end if;
-			when BUSY_DEC_OUTPUT =>
-				NEXT_STATUS <= BUSY_DEC_OUTPUT;
+			when BUSY_DEC_OUTPUT1 =>
+				NEXT_STATUS <= BUSY_DEC_OUTPUT2;
+			when BUSY_DEC_OUTPUT2 =>
+				NEXT_STATUS <= BUSY_DEC_OUTPUT1;
 			when others => 
 				NEXT_STATUS <= STATUS;
 		end case;
@@ -133,104 +125,95 @@ begin
 		begin
 			if rst = '1' then
 				STATUS <= IDLE;
-				data_output0 <= ZERO32;
-				data_output1 <= ZERO32;
+				data_output <= ZERO;
 				output_ready <= '0';
-				key0 <= ZERO32;
-				key1 <= ZERO32;
-				key2 <= ZERO32;
-				key3 <= ZERO32;
-				text0 <= ZERO32;
-				text1 <= ZERO32;
-				counter <= "0000000";
-				sum <= ZERO64;
+				KEY0 <= ZERO;
+				KEY1 <= ZERO;
+				KEY2 <= ZERO;
+				KEY3 <= ZERO;
+				TEXT0 <= ZERO;
+				TEXT1 <= ZERO;
+				COUNTER <= "000000";
+				SUM <= ZERO;
 			else
 				STATUS <= NEXT_STATUS;
 				case NEXT_STATUS is
 					when IDLE => 
-						data_output0 <= ZERO32;
-						data_output1 <= ZERO32;
+						data_output <= ZERO;
 						output_ready <= '0';
 					when BUSY_KEY1 => 
-						KEY0 <= key_input0;
-						KEY1 <= key_input1;
+						KEY0 <= data_input(31 downto 0);
+						KEY1 <= data_input(63 downto 32);
 					when BUSY_KEY2 => 
-						KEY2 <= key_input0;
-						KEY3 <= key_input1;
+						KEY2 <= data_input(31 downto 0);
+						KEY3 <= data_input(63 downto 32);
 					when BUSY_ENC_INPUT => 
-						TEXT0 <= text_input0;
-						TEXT1 <= text_input1;
+						TEXT0 <= data_input(31 downto 0);
+						TEXT1 <= data_input(63 downto 32);
 					when BUSY_ENC1 => 
-						case (sum and THREE) is
-							when ZERO64 => 
-							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY0));
+						case (SUM and THREE) is
+							when ZERO => 
+							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY0));
 							when ONE => 
-							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY1));
+							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY1));
 							when TWO => 
-							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY2));
-							when THREE => 
-							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY3));
-							when others =>
-							report "Error during encryption";
+							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY2));
+							when others => 
+							TEXT0 <= TEXT0 + ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY3));
 						end case;
 						SUM <= SUM + delta; 
 					when BUSY_ENC2 => 
-						case ((sum srl 11) and THREE) is
-							when ZERO64 => 
-							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY0));
+						case ((SUM srl 11) and THREE) is
+							when ZERO => 
+							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY0));
 							when ONE => 
-							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY1));
+							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY1));
 							when TWO => 
-							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY2));
-							when THREE => 
-							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY3));
-							when others =>
-							report "Error during encryption";
+							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY2));
+							when others => 
+							TEXT1 <= TEXT1 + ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY3));
 						end case;
-						counter <= counter + 1;
-					when BUSY_ENC_OUTPUT =>
-						data_output0 <= TEXT0;
-						data_output1 <= TEXT1;
+						COUNTER <= COUNTER + 1;
+					when BUSY_ENC_OUTPUT1 =>
+						data_output <= TEXT0;
 						output_ready <= '1';
+					when BUSY_ENC_OUTPUT2 =>
+						data_output <= TEXT1;
 					when BUSY_DEC_INPUT => 
-						TEXT0 <= text_input0;
-						TEXT1 <= text_input1;
-						sum <= delta * SIZE;
+						TEXT0 <= data_input(31 downto 0);
+						TEXT1 <= data_input(63 downto 32);
+						SUM <= "11000110111011110011011100100000";
 					when BUSY_DEC1 => 
-						case ((sum srl 11) and THREE) is
-							when ZERO64 => 
-							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY0));
+						case ((SUM srl 11) and THREE) is
+							when ZERO => 
+							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY0));
 							when ONE => 
-							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY1));
+							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY1));
 							when TWO => 
-							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY2));
-							when THREE => 
-							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (sum(31 downto 0) + KEY3));
-							when others =>
-							report "Error during encryption";
+							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY2));
+							when others => 
+							TEXT1 <= TEXT1 - ((((TEXT0 sll 4) xor (TEXT0 srl 5)) + TEXT0) xor (SUM + KEY3));
 						end case;
-						sum <= sum - delta;
+						SUM <= SUM - delta;
 					when BUSY_DEC2 =>
-						case (sum and THREE) is
-							when ZERO64 => 
-							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY0));
+						case (SUM and THREE) is
+							when ZERO => 
+							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY0));
 							when ONE => 
-							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY1));
+							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY1));
 							when TWO => 
-							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY2));
-							when THREE => 
-							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (sum(31 downto 0) + KEY3));
-							when others =>
-							report "Error during encryption";
+							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY2));
+							when others => 
+							TEXT0 <= TEXT0 - ((((TEXT1 sll 4) xor (TEXT1 srl 5)) + TEXT1) xor (SUM + KEY3));
 						end case;
-						counter <= counter + 1;
-					when BUSY_DEC_OUTPUT => 
-						data_output0 <= TEXT0;
-						data_output1 <= TEXT1;
+						COUNTER <= COUNTER + 1;
+					when BUSY_DEC_OUTPUT1 => 
+						data_output <= TEXT0;
 						output_ready <= '1';
+					when BUSY_DEC_OUTPUT2=>
+						data_output <= TEXT1;
 					when others => 
-						data_output0 <= ZERO32;
-						data_output1 <= ZERO32;
+						data_output <= ZERO;
 				end case;
 			end if;
 		end process;
--- stimuli.do
+++ stimuli.do
@@ -1,3 +1,5 @@
+# Simulation script for XTEA
+# by Matteo Iervasi
 vsim work.xtea
 
 add wave clk
@@ -5,97 +7,91 @@ add wave rst
 add wave input_ready
 add wave output_ready
 add wave mode
-add wave -radix hexadecimal text_input0
-add wave -radix hexadecimal text_input1
-add wave -radix hexadecimal key_input0
-add wave -radix hexadecimal key_input1
-add wave -radix hexadecimal data_output0
-add wave -radix hexadecimal data_output1
+add wave -radix hexadecimal -color yellow data_input
+add wave -radix hexadecimal -color yellow data_output
 
-add wave -radix hexadecimal key0
-add wave -radix hexadecimal key1
-add wave -radix hexadecimal key2
-add wave -radix hexadecimal key3
-add wave -radix hexadecimal status
+add wave -radix hexadecimal -color red key0
+add wave -radix hexadecimal -color red key1
+add wave -radix hexadecimal -color red key2
+add wave -radix hexadecimal -color red key3
+add wave -radix hexadecimal -color red text0
+add wave -radix hexadecimal -color red text1
+add wave -radix unsigned -color blue status
 
 # Set up 100mhz clock
 force clk 0 0, 1 5ns -r 10ns
 
-# Run encryption
+## START ENCRYPTION
 force rst 0
-force text_input0 16#12345678
-force text_input1 16#9abcdeff
-# Input keys
-force key_input0 16#6a1d78c8
-force key_input1 16#8c86d67f
-force input_ready 1
-run 5ns
-
-force key_input0 16#2a65bfbe
-force key_input1 16#b4bd6e46
 force mode 0
+# Input keys
+force data_input 16#8c86d67f6a1d78c8
+force input_ready 1
+run 5ns
+force data_input 16#b4bd6e462a65bfbe
+run 5ns
+# Input text
+force data_input 16#9abcdeff12345678
 force input_ready 1
 run 500ns
+## END ENCRYPTION
 
-# Reset the chip
+## RESET THE CHIP
 force rst 1
 force input_ready 0
 run 50ns
 
-# Run decryption
+## START DECRYPTION
 force rst 0
-force text_input0 16#99bbb92b
-force text_input1 16#3ebd1644
-# Input keys
-force key_input0 16#6a1d78c8
-force key_input1 16#8c86d67f
-force input_ready 1
-run 5ns
-
-force key_input0 16#2a65bfbe
-force key_input1 16#b4bd6e46
 force mode 1
+# Input keys
+force data_input 16#8c86d67f6a1d78c8
+force input_ready 1
+run 5ns
+force data_input 16#b4bd6e462a65bfbe
+run 5ns
+# Input text
+force data_input 16#3ebd164499bbb92b
 force input_ready 1
 run 500ns
+## END DECRYPTION
 
-# Reset the chip
+## RESET THE CHIP
 force rst 1
 force input_ready 0
 run 50ns
 
-# Run encryption
+## RUN ENCRYPTION
 force rst 0
-force text_input0 16#fd0fe0f1
-force text_input1 16#ca2ea80a
-# Input keys
-force key_input0 16#6a1d78c8
-force key_input1 16#8c86d67f
-force input_ready 1
-run 5ns
-
-force key_input0 16#2a65bfbe
-force key_input1 16#b4bd6e46
 force mode 0
+# Input keys
+force data_input 16#8c86d67f6a1d78c8
+force input_ready 1
+run 5ns
+force data_input 16#b4bd6e462a65bfbe
+run 5ns
+# Input text
+force data_input 16#ca2ea80afd0fe0f1
 force input_ready 1
 run 500ns
+## END ENCRYPTION
 
-# Reset the chip
+## RESET THE CHIP
 force rst 1
 force input_ready 0
 run 50ns
 
-# Run decryption
+# RUN DECRYPTION
 force rst 0
-force text_input0 16#1dc4dc42
-force text_input1 16#11f66792
+force mode 1
 # Input keys
-force key_input0 16#6a1d78c8
-force key_input1 16#8c86d67f
+force data_input 16#8c86d67f6a1d78c8
 force input_ready 1
 run 5ns
-
-force key_input0 16#2a65bfbe
-force key_input1 16#b4bd6e46
-force mode 1
+force data_input 16#b4bd6e462a65bfbe
+run 5ns
+# Input text
+force data_input 16#11f667921dc4dc42
 force input_ready 1
 run 500ns
+## END DECRYPTION
